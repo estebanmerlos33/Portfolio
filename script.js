1cc8bb7bc60eab7f36d8d2d9007dfee3ee3f87ea
Esteban Merlos <61000414+estebanmerlos33@users.noreply.github.com>
2024-10-05 17:47:59 -0300

    Removido bug scroll

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 history.scrollRestoration = "manual";
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("show");
-    } else {
-      entry.target.classList.remove("show");
-    }
-  });
-});
-
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => observer.observe(el));
-
 window.onload = function () {
   window.scrollTo(0, 0);
 };
